90cb62b682d2a401d0d945fc23a82e7f5591fd77
Keris <kerisve@hotmail.com>
2014-10-01 10:37:08 +0200

    import.sql + abstractDao + abstractEntity

---
+++ src/main/java/com/realdolmen/travel/script/Runner.java
@@ -0,0 +1,11 @@
+package com.realdolmen.travel.script;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,90 @@
-insert into user(firstName, lastName, dateOfBirth) values('Jimi', 'Hendrix', '1946-11-26');
-insert into user(firstName, lastName, dateOfBirth) values('Janis', 'Joplin', '1983-09-12');
-insert into user(firstName, lastName, dateOfBirth) values('Jim', 'Morrison', '2072-06-16');
+insert into region(id, code, name) values(1,'EU', 'Europe');
+insert into region(id, code, name) values(2,'AS', 'Asia');
+insert into region(id, code, name) values(3,'AF', 'Africa');
+insert into region(id, code, name) values(4,'AN', 'Antarctica');
+insert into region(id, code, name) values(5,'OC', 'Oceania');
+insert into region(id, code, name) values(6,'NA', 'North America');
+insert into region(id, code, name) values(7,'SA', 'South America');
+
+insert into location(id, code, name, region_fk) values(1, 'LON', 'London', 1);
+insert into location(id, code, name, region_fk) values(2, 'BRU', 'Brussels', 1);
+insert into location(id, code, name, region_fk) values(3, 'PAR', 'Paris', 1);
+insert into location(id, code, name, region_fk) values(4, 'MAD', 'Madrid', 1);
+insert into location(id, code, name, region_fk) values(5, 'MIL', 'Milan', 1);
+insert into location(id, code, name, region_fk) values(6, 'PRA', 'Prague', 1);
+insert into location(id, code, name, region_fk) values(7, 'DUB', 'Dublin', 1);
+insert into location(id, code, name, region_fk) values(8, 'LON', 'London', 1);
+insert into location(id, code, name, region_fk) values(9, 'BER', 'Berlin', 1);
+insert into location(id, code, name, region_fk) values(10, 'ROM', 'Rome', 1);
+insert into location(id, code, name, region_fk) values(11, 'STO', 'Stockholm', 1);
+insert into location(id, code, name, region_fk) values(12, 'MAR', 'Marseille', 1);
+insert into location(id, code, name, region_fk) values(13, 'AMS', 'Amsterdam', 1);
+insert into location(id, code, name, region_fk) values(14, 'ZAG', 'Zagreb', 1);
+insert into location(id, code, name, region_fk) values(15, 'RIG', 'Riga', 1);
+insert into location(id, code, name, region_fk) values(16, 'GLA', 'Glasgow', 1);
+insert into location(id, code, name, region_fk) values(17, 'LIS', 'Lisbon', 1);
+insert into location(id, code, name, region_fk) values(18, 'MAN', 'Manchester', 1);
+insert into location(id, code, name, region_fk) values(19, 'TOU', 'Toulouse', 1);
+insert into location(id, code, name, region_fk) values(20, 'GEN', 'Genoa', 1);
+
+insert into location(id, code, name, region_fk) values(21, 'NYC', 'New York City', 6);
+insert into location(id, code, name, region_fk) values(22, 'LA', 'Los Angeles', 6);
+insert into location(id, code, name, region_fk) values(23, 'TOR', 'Toronto', 6);
+insert into location(id, code, name, region_fk) values(24, 'CHI', 'Chicago', 6);
+insert into location(id, code, name, region_fk) values(25, 'HOU', 'Houston', 6);
+insert into location(id, code, name, region_fk) values(26, 'PHO', 'Phoenix', 6);
+insert into location(id, code, name, region_fk) values(27, 'MEX', 'Mexico City', 6);
+insert into location(id, code, name, region_fk) values(28, 'HAV', 'Havana', 6);
+
+insert into location(id, code, name, region_fk) values(29, 'BEI', 'Beijing', 2);
+insert into location(id, code, name, region_fk) values(30, 'HON', 'Hong Kong', 2);
+insert into location(id, code, name, region_fk) values(31, 'SHA', 'Shangai', 2);
+insert into location(id, code, name, region_fk) values(32, 'SET', 'SETO', 2);
+insert into location(id, code, name, region_fk) values(33, 'TOY', 'Toyota', 2);
+
+insert into location(id, code, name, region_fk) values(34, 'COF', 'Coffee Bay', 3);
+insert into location(id, code, name, region_fk) values(35, 'BAL', 'Balfour', 3);
+insert into location(id, code, name, region_fk) values(36, 'QUE', 'Queenstown', 3);
+
+insert into location(id, code, name, region_fk) values(37, 'LIM', 'Lima', 7);
+insert into location(id, code, name, region_fk) values(38, 'RIO', 'Rio de Janeiro', 7);
+insert into location(id, code, name, region_fk) values(39, 'SAO', 'Sao Paulo', 7);
+insert into location(id, code, name, region_fk) values(40, 'SAL', 'Salvador', 7);
+
+insert into location(id, code, name, region_fk) values(41, 'SYD', 'Sydney', 5);
+insert into location(id, code, name, region_fk) values(41, 'ALB', 'Albury', 5);
+insert into location(id, code, name, region_fk) values(41, 'NEW', 'Newcastle', 5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
