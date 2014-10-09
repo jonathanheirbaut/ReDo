e0f500ad9033c8f608901ea127e709485041917d
Jonathan Heirbaut <jonathan.heirbaut@realdolmen.com>
2014-10-09 12:10:50 +0200

    Search trip

--- src/main/java/com/realdolmen/travel/controller/TripController.java
+++ src/main/java/com/realdolmen/travel/controller/TripController.java
@@ -1,5 +1,7 @@
 package com.realdolmen.travel.controller;
 
+import com.realdolmen.travel.domain.Location;
+import com.realdolmen.travel.domain.Region;
 import com.realdolmen.travel.domain.Trip;
 import com.realdolmen.travel.service.TripService;
 
@@ -7,6 +9,7 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -18,6 +21,14 @@ public class TripController {
     @Inject
     private TripService tripService;
     private List<Trip> trips;
+
+    private Region departureRegion;
+    private Location departureLocation;
+    private Region destinationRegion;
+    private Location destinationLocation;
+    private Date departureDate;
+    private Date returnDate;
+    private int numberOfPersons;
 
 
     @PostConstruct
@@ -33,7 +44,67 @@ public class TripController {
         return null;
     }
 
+    public void onDepartureRegionChange() {
+
+    }
+
     public List<Trip> getTrips() {
         return trips;
+    }
+
+    public Region getDepartureRegion() {
+        return departureRegion;
+    }
+
+    public void setDepartureRegion(Region departureRegion) {
+        this.departureRegion = departureRegion;
+    }
+
+    public Location getDepartureLocation() {
+        return departureLocation;
+    }
+
+    public void setDepartureLocation(Location departureLocation) {
+        this.departureLocation = departureLocation;
+    }
+
+    public Region getDestinationRegion() {
+        return destinationRegion;
+    }
+
+    public void setDestinationRegion(Region destinationRegion) {
+        this.destinationRegion = destinationRegion;
+    }
+
+    public Location getDestinationLocation() {
+        return destinationLocation;
+    }
+
+    public void setDestinationLocation(Location destinationLocation) {
+        this.destinationLocation = destinationLocation;
+    }
+
+    public Date getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(Date departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public int getNumberOfPersons() {
+        return numberOfPersons;
+    }
+
+    public void setNumberOfPersons(int numberOfPersons) {
+        this.numberOfPersons = numberOfPersons;
     }
 }
--- src/main/java/com/realdolmen/travel/repository/LocationRepository.java
+++ src/main/java/com/realdolmen/travel/repository/LocationRepository.java
@@ -1,10 +1,18 @@
 package com.realdolmen.travel.repository;
 
 import com.realdolmen.travel.domain.Location;
+import com.realdolmen.travel.domain.Region;
 import com.realdolmen.travel.repository.AbstractRepository;
 
 import javax.ejb.Stateless;
+import javax.persistence.TypedQuery;
+import java.util.Collection;
 
 @Stateless
 public class LocationRepository extends AbstractRepository<Location> {
+    public Collection<Location> getLocationsByRegion(Region region) {
+        TypedQuery<Location> query = em.createQuery("SELECT l FROM Location l WHERE l.region = :region", Location.class);
+        query.setParameter("region", region);
+        return query.getResultList();
+    }
 }
